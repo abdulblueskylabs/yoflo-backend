0e34b7ee7c765f38dac14d0399906e16542ae2ff
Abdul <abdul@blueskylabs.com.au>
2022-05-11 16:14:42 +0800

    prefix Added in routes

--- routes/api.php
+++ routes/api.php
@@ -1,52 +1,56 @@
 <?php
 
-  use Illuminate\Support\Facades\Route;
-  use App\Http\Controllers\API\Auth\LoginController;
-  use App\Http\Controllers\API\Auth\RegistrationController;
-  use App\Http\Controllers\API\Auth\ResetPasswordController;
-  use App\Http\Controllers\API\Auth\forgotPasswordController;
-  use App\Http\Controllers\API\Subscription\SubscriptionController;
-  use \App\Http\Controllers\API\UserSubscription\UserSubscriptionController;
-  use \App\Http\Controllers\API\UserProfile\UserProfileController;
+use App\Http\Controllers\Admin\Auth\VerificationController;
+use App\Http\Controllers\API\Auth\forgotPasswordController;
+use App\Http\Controllers\API\Auth\LoginController;
+use App\Http\Controllers\API\Auth\RegistrationController;
+use App\Http\Controllers\API\Auth\ResetPasswordController;
+use App\Http\Controllers\API\Subscription\SubscriptionController;
+use App\Http\Controllers\API\UserProfile\UserProfileController;
+use App\Http\Controllers\API\UserSubscription\UserSubscriptionController;
+use Illuminate\Support\Facades\Route;
 
-  /*
-  |--------------------------------------------------------------------------
-  | API Routes
-  |--------------------------------------------------------------------------
-  |
-  | Here is where you can register API routes for your application. These
-  | routes are loaded by the RouteServiceProvider within a group which
-  | is assigned the "api" middleware group. Enjoy building your API!
-  |
-  */
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
 
-  /*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-      return $request->user();
-  });*/
+// All routes have default prefix => api
 
-  Route::post('user/sign-up', [RegistrationController::class, 'register']);
-  Route::post('user/login', [LoginController::class, 'login']);
-  Route::post('user/request-password', [forgotPasswordController::class, 'forgotPassword']);
-  Route::post('user/reset-email-password', [forgotPasswordController::class, 'resetPassword']);
-  Route::get('subscriptions', [SubscriptionController::class,'index']);
+Route::prefix('user')->group(function () {
 
-  Route::get('email/verify/{id}', [\App\Http\Controllers\Auth\VerificationController::class,'verify'])->name('verification.verify');
-  Route::get('email/resend', [\App\Http\Controllers\Auth\VerificationController::class,'resend'])->name('verification.resend');
+  Route::post('sign-up', [RegistrationController::class, 'register']);
+  Route::post('login', [LoginController::class, 'login']);
+  Route::post('request-password', [forgotPasswordController::class, 'forgotPassword']);
+  Route::post('reset-email-password', [forgotPasswordController::class, 'resetPassword']);
 
-  Route::group(['middleware' => ['auth:sanctum', 'role:user']], function() {
-
-    // Auth routes
-    Route::post('/user/logout', [LoginController::class, 'logout']);
-    Route::post('user/change-password', [ResetPasswordController::class, 'changePassword']);
+});
 
 
+Route::get('subscriptions', [SubscriptionController::class, 'index']);
 
-    // User-Subscriptions routes
-    Route::get('user/subscription', [UserSubscriptionController::class, 'index']);
-    Route::put('user/subscription/{id}', [UserSubscriptionController::class, 'update']);
+Route::get('email/verify/{id}', [VerificationController::class, 'verify'])->name('verification.verify');
+Route::get('email/resend', [VerificationController::class, 'resend'])->name('verification.resend');
 
-    // User-Profile routes
-    Route::get('user/profile', [UserProfileController::class, 'index']);
-    Route::put('user/profile', [UserProfileController::class, 'update']);
+Route::group(['prefix' => 'api/user', 'middleware' => ['auth:sanctum', 'role:user']], function () {
 
-  });
+  // Auth routes
+  Route::post('logout', [LoginController::class, 'logout']);
+  Route::post('change-password', [ResetPasswordController::class, 'changePassword']);
+
+
+  // User-Subscriptions routes
+  Route::get('subscription', [UserSubscriptionController::class, 'index']);
+  Route::put('subscription/{id}', [UserSubscriptionController::class, 'update']);
+
+  // User-Profile routes
+  Route::get('profile', [UserProfileController::class, 'index']);
+  Route::put('profile', [UserProfileController::class, 'update']);
+
+});
